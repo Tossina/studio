3a9fb3a9567864fafd2f7ef203ee318eabf49d06
Development Technocraft <technocraft.devloppement@gmail.com>
2025-12-09 22:10:28 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8846791207-417da",
+  "appId": "1:377846561876:web:eddce6e004ec3ca080c1c1",
+  "apiKey": "AIzaSyDVpGUMzSFPGDlc5-jghOA9-DzA4ThbuhY",
+  "authDomain": "studio-8846791207-417da.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "377846561876"
+};
